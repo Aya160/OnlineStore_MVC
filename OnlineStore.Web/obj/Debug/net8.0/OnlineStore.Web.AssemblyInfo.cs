722d4030acadbdad8b15c693e50a92fc10f480c1
Aya Hamad <aya408757@gmail.com>
2024-06-28 23:02:28 +0200

    Handel Buttons

--- OnlineStore.Web/obj/Debug/net8.0/OnlineStore.Web.AssemblyInfo.cs
+++ OnlineStore.Web/obj/Debug/net8.0/OnlineStore.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47f4cffc3a5f27d16688b2ce7611a84232abd013")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+488d619f497c2beafc8fe97152e9fbd4fcdd3f5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
