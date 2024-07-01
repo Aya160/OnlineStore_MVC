60d4aaad3aeecdf2083a814586839cf689509595
NadaMastafa <nm8178182@gmail.com>
2024-07-01 21:20:48 +0300

    No Commit

--- OnlineStore.Web/obj/Debug/net8.0/OnlineStore.Web.AssemblyInfo.cs
+++ OnlineStore.Web/obj/Debug/net8.0/OnlineStore.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+917e6295de7dd5b8b6b3712b9b4c7564905d2fea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2c017c05e5240d279629376848c07e9e03cb1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
